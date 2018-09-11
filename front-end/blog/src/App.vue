5f82152e524abb815012923c016f8de8872904c1
weizb <wzb19960208@outlook.com>
2018-09-11 17:22:15 +0800

    '完成了博客首页和header的搭建'

---
+++ front-end/blog/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div id="container">
+      <home-header></home-header>
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
+    </div>
+    <div id="bg"></div>
+  </div>
+</template>
+
+<script>
+import HomeHeader from "./components/header/Header";
+export default {
+  name: 'App',
+  components: {HomeHeader}
+}
+</script>
+
+<style scoped>
+
+  #app{
+    width: 100%;
+  }
+
+  #container{
+    width: 100%;
+    height: 1500px;
+    background-color: rgba(255,255,255,0.8);
+  }
+
+  #bg{
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: url("https://blog-1252736437.cos.ap-shanghai.myqcloud.com/background.png") fixed repeat-y;
+    background-size: 100%;
+    filter: blur(2px);
+    z-index: -1;
+  }
+</style>
---
+++ front-end/blog/src/components/header/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="header">
+    <div class="img-wrap">
+      <img src="https://blog-1252736437.cos.ap-shanghai.myqcloud.com/touxiang.jpg" class="img-item">
+    </div>
+    <div class="title">
+      <p>Ben</p>
+    </div>
+    <div class="desc">
+      <p>Master of NJU/Bachelor of UESTC/Enjoy Coding</p>
+    </div>
+    <div class="item-list">
+        <router-link
+          tag="i"
+          :to="'/'"
+          title="回到首页"
+          class="iconfont item">
+          &#xe600;
+        </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'home-header'
+  }
+</script>
+
+<style scoped>
+
+  .header{
+    width: 100%;
+    height: 21.875rem;
+    padding: .8rem 0 .4rem;
+  }
+
+  .img-wrap{
+    width: 100%;
+  }
+
+  .img-item{
+    display: block;
+    margin: 0 auto;
+    height: 2.56rem;
+    width: 2.56rem;
+    border-radius: 50%;
+  }
+
+  .title{
+    margin-top: .4rem;
+    margin-bottom: .4rem;
+    font-weight: 700;
+    width: 100%;
+    text-align: center;
+    color: #555;
+  }
+
+  .desc{
+    margin-bottom: .4rem;
+    color:#65737e;
+    font-size: 14px;
+    text-align: center;
+  }
+
+  .item-list{
+    width: 100%;
+    text-align: center;
+  }
+
+  .item{
+    display: inline-block;
+    font-size: .48rem;
+    cursor: pointer;
+  }
+</style>
